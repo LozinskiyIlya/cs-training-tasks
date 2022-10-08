f3ba8023c7171d16d654d3745f8a1206e3d3087f
Ilya <inbtrhope@gmail.com>
2022-10-08 10:59:15 +0300

    edits (array of arrays)

--- Homework/Task1/Program.cs
+++ Homework/Task1/Program.cs
@@ -55,6 +55,21 @@ namespace Task1
             return matrix;
         }
 
+        private static double[][] GenerateMatrixArrayOfArrays(int n, int m)
+        {
+            var random = new Random(Guid.NewGuid().GetHashCode());
+            var matrix = new double[n][];
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = new double[m];
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i][j] = random.NextDouble();
+                }
+            }
+            return matrix;
+        }
+
 
         private static double[,] MultiplyMatrices(double[,] a, double[,] b)
         {
@@ -74,6 +89,25 @@ namespace Task1
             return c;
         }
 
+        private static double[][] MultiplyMatricesArrayOfArrays(double[][] a, double[][] b)
+        {
+            var height = a.GetLength(0);
+            var width = b[0].GetLength(0);
+            var c = new double[height][];
+            for (int i = 0; i < height; i++)
+            {
+                c[i] = new double[width];
+                for (int j = 0; j < width; j++)
+                {
+                    for (int r = 0; r < a[0].GetLength(0); r++)
+                    {
+                        c[i][j] += a[i][r] * b[r][j];
+                    }
+                }
+            }
+            return c;
+        }
+
         public static void Main(string[] args)
         {
             var dim = ParseArgs(args);
@@ -82,12 +116,24 @@ namespace Task1
             var opCnt = Math.Pow(dim.Max(), 3) * 2;
             var a = GenerateMatrix(dim[0], dim[1]);
             var b = GenerateMatrix(dim[1], dim[2]);
+            Console.WriteLine("Rectangular Array:");
             sw.Start();
             MultiplyMatrices(a, b);
             sw.Stop();
             Console.WriteLine("Finished, elapsed time, ms: " + sw.ElapsedMilliseconds);
             Console.WriteLine("Perfomance, GFlops: " + opCnt * 1000 / sw.ElapsedMilliseconds);
-            //commented for tests
+
+            Console.WriteLine("Array of Arrays:");
+            var a1 = GenerateMatrixArrayOfArrays(dim[0], dim[1]);
+            var b1 = GenerateMatrixArrayOfArrays(dim[1], dim[2]);
+            sw.Restart();
+            MultiplyMatricesArrayOfArrays(a1, b1);
+            sw.Stop();
+            Console.WriteLine("Finished, elapsed time, ms: " + sw.ElapsedMilliseconds);
+            Console.WriteLine("Perfomance, GFlops: " + opCnt * 1000 / sw.ElapsedMilliseconds);
+
+
+            //comment for tests
             //Console.WriteLine("\nPress any key to close");
             //Console.ReadKey();
         }
