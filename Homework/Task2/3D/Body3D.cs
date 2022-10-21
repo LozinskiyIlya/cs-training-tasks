1b80b15c48edbc4f280f8fc5ac0b466b84790252
Ilya <inbtrhope@gmail.com>
2022-10-21 11:43:28 +0300

    Body3D

---
+++ Homework/Task2/3D/Body3D.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Homework.Task2
+{
+    public abstract class Body3D
+    {
+        public abstract decimal Volume();
+        public abstract decimal SurfaceArea();
+        public abstract decimal Perimeter();
+    }
+}
---
+++ Homework/Task2/3D/Parallelepiped.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Homework.Task2
+{
+    public class Parallelepiped : Body3D
+    {
+        private decimal l;
+        private decimal w;
+        private decimal h;
+
+        public Parallelepiped(decimal length, decimal width, decimal height)
+        {
+            l = length;
+            w = width;
+            h = height;
+        }
+        public decimal Length { get => l; set => l = value; }
+        public decimal Width { get => w; set => w = value; }
+        public decimal Height { get => h; set => h = value; }
+
+        public override decimal Perimeter()
+        {
+           return 4 * (l + w + h);
+        }
+
+        public override decimal SurfaceArea()
+        {
+            return 2 * (l * w + w * h + l * h);
+        }
+
+        public override decimal Volume()
+        {
+            return l * w * h;
+        }
+    }
+}
