997b5519d35f84c80ea947fb51e561ca31d5ae37
Ilya <inbtrhope@gmail.com>
2022-11-09 15:13:48 +0300

    Task 6 start

---
+++ Homework/Task6/Dictionary.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Homework.Task6
+{
+    interface IDict
+    {
+        void Add(string word, string translation);
+        HashSet<string> GetTranslations(string word);
+        int Size();
+
+    }
+    public class Dict : IDict
+    {
+        private static Dict instance;
+        private Dictionary<string, HashSet<string>> translations;
+        private Dict()
+        {
+            initDict();
+        }
+        public static Dict Instance()
+        {
+            if (instance == null)
+            {
+                instance = new Dict();
+            }
+            return instance;
+        }
+        public void Add(string word, string translation)
+        {
+            Add(word, translation, true);
+        }
+
+        public HashSet<string> GetTranslations(string word)
+        {
+            HashSet<string> outSet;
+            return translations.TryGetValue(word, out outSet) ?
+                new HashSet<string>(outSet) :
+                new HashSet<string>();
+        }
+
+        public int Size()
+        {
+            return translations.Count;
+        }
+
+        public void Add(string word, string translation, bool addReverse)
+        {
+            if (translations.ContainsKey(word))
+            {
+                HashSet<string> existing;
+                translations.TryGetValue(word, out existing);
+                translations.Remove(word);
+                existing.Add(translation);
+                translations.Add(word, existing);
+            }
+            else
+            {
+                translations.Add(word, new HashSet<string> { translation });
+            }
+
+            if (addReverse)
+            {
+                Add(translation, word, false);
+            }
+        }
+
+        private void initDict()
+        {
+            translations = new Dictionary<string, HashSet<string>>();
+            translations.Add("идти", new HashSet<string> { "go", "walk", "move" });
+            translations.Add("go", new HashSet<string> { "идти" });
+            translations.Add("walk", new HashSet<string> { "идти", "гулять" });
+            translations.Add("move", new HashSet<string> { "идти", "двигать" });
+            translations.Add("гулять", new HashSet<string> { "walk" });
+            translations.Add("двигать", new HashSet<string> { "move" });
+            translations.Add("дом", new HashSet<string> { "house", "home" });
+            translations.Add("house", new HashSet<string> { "дом" });
+            translations.Add("home", new HashSet<string> { "дом" });
+            translations.Add("schedule", new HashSet<string> { "расписание", "график" });
+            translations.Add("расписание", new HashSet<string> { "schedule" });
+            translations.Add("график", new HashSet<string> { "schedule" });
+        }
+    }
+}
---
+++ HomeworkTests/Task6ServiceUT.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Homework.Task6;
+
+namespace HomeworkTests
+{
+    [TestClass]
+    public class Task6ServiceUT
+
+    {
+        // **************** //
+        //  Initialization  //
+        // **************** //
+
+       // [TestMethod("Should init dictionary")]
+        public void ShouldInitDict()
+        {
+            var d = Dict.Instance();
+            Assert.AreNotEqual(0, d.Size());
+        }
+
+       // [TestMethod("Should have translation for default words")]
+        public void ShouldHaveTranslationsForDefaults()
+        {
+            var d = Dict.Instance();
+            Assert.AreEqual("идти", d.GetTranslations("go"));
+        }
+    }
+}
