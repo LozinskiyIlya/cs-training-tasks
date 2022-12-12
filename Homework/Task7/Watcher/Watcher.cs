3a223cf1b3a95fdb3340fb5f2ea88dfa0f6b105d
ilya <inbtrhope@gmail.com>
2022-12-12 12:27:36 +0530

    cleanup

--- Homework/Task7/Watcher/Watcher.cs
+++ Homework/Task7/Watcher/Watcher.cs
@@ -125,7 +125,6 @@ namespace Homework.Task7
 
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
             Dispose(disposing: true);
             GC.SuppressFinalize(this);
         }
